cb501bd668e5da166e1d6f921a084859b97b6f01
cyng93 <cyng93@gmail.com>
2016-12-02 00:22:39 +0800

    [bugfix] logout user if account was deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
+    if session[:user_id] && !User.exists?(session[:user_id])
+      session[:user_id] = nil
+    end
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
