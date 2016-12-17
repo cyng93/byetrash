814276d0a145aaf87d7509c26761b772de941a24
CHING-YI NG <cyng93@gmail.com>
2016-12-18 01:56:15 +0800

    [feature] user trash history
    - User can now see their thrown waste history
    - Move conclusion from thrownwaste#index to new model conclusion

---
+++ app/controllers/conclusion_controller.rb
@@ -0,0 +1,5 @@
+class ConclusionController < ApplicationController
+  def index
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   post 'users' => 'users#create'
 
+  get 'conclusion/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
