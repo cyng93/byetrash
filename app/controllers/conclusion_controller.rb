25312c2b0c05ebbabfae081fab3cdbde2a9ece99
CHING-YI NG <cyng93@gmail.com>
2017-01-03 23:50:35 +0800

    [update] finish up conclusion page
    - show user conclusion/result of the past two week

--- app/controllers/conclusion_controller.rb
+++ app/controllers/conclusion_controller.rb
@@ -1,5 +1,13 @@
 class ConclusionController < ApplicationController
   def index
+    @user = current_user
+    @week1_trash = @user.score_week1
+    @week2_trash = @user.score_week2
+    if @week1_trash > @week2_trash
+      @reduce_percent = (@week1_trash - @week2_trash)/(@week1_trash*1.0)*100
+    else
+      @reduce_percent = (@week2_trash - @week1_trash)/(@week1_trash*1.0)*100
+    end
   end
 end
 
